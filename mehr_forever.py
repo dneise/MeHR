55babee7177f5fb338afa0d663d7517ecfa80217
Dominik Neise <neised@phys.ethz.ch>
2018-06-26 14:37:51 +0200

    make "repeat" more verbose

--- mehr_forever.py
+++ mehr_forever.py
@@ -8,32 +8,51 @@ from mehr import (
     mews_report_to_report_rows,
     write_excel_output_file
 )
+from tqdm import trange
 
 here = os.path.dirname(os.path.realpath(__file__))
+
+next_execution = 0
+
+
+def wait_for_next_execution(period=timedelta(hours=24).total_seconds()):
+    global next_execution
+    current_time = time.time()
+    if current_time >= next_execution:
+        next_execution = current_time + period
+        return
+    else:
+        minutes_until = (next_execution - current_time) // 60
+        print(
+            '{} minutes until next report to be made... waiting'.format(
+                minutes_until
+            )
+        )
+        for i in trange(minutes_until * 6):
+            if current_time >= next_execution:
+                next_execution = current_time + period
+                return
+            time.sleep(10)
 
 
 def repeat():
     configs = load_config(here)
-    period = timedelta(hours=24).total_seconds()
-    next_execution = 0
+
     while True:
-        current_time = time.time()
-        if current_time >= next_execution:
-            for config in configs:
-                config = SimpleNamespace(**config)
-                mews_report = get_started_reservations_yesterday(config)
-                rows = mews_report_to_report_rows(mews_report)
-                now = datetime.now()
-                outpath = os.path.join(
-                    config.OutFolder,
-                    now.strftime(config.FileName)
-                )
-                write_excel_output_file(
-                    rows,
-                    outpath=outpath
-                )
-            next_execution = current_time + period
-        time.sleep(60)
+        wait_for_next_execution()
+        for config in configs:
+            config = SimpleNamespace(**config)
+            mews_report = get_started_reservations_yesterday(config)
+            rows = mews_report_to_report_rows(mews_report)
+            now = datetime.now()
+            outpath = os.path.join(
+                config.OutFolder,
+                now.strftime(config.FileName)
+            )
+            write_excel_output_file(
+                rows,
+                outpath=outpath
+            )
 
 
 if __name__ == '__main__':
