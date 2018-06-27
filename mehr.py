16d9ec392949355447ccd409eb8df9b320c65151
Dominik Neise <neised@phys.ethz.ch>
2018-06-27 09:17:13 +0200

    remove config.Name (not needed); propose filename similar to M-fidelio

--- mehr.py
+++ mehr.py
@@ -38,8 +38,6 @@ def repeat():
         try:
             wait_for_next_execution()
             for config in configs:
-                print('Working on Hotel: {}'.format(
-                    config.Name))
                 mews_report, start_time = reservations_getAll(config)
                 rows = mews_report_to_report_rows(mews_report)
                 outpath = os.path.join(
