b478be5670144066c41d5b7d8a0496694552a626
Dominik Neise <neised@phys.ethz.ch>
2018-06-28 15:15:17 +0200

    let's call this version 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MeHR',
-    version='0.0.4',
+    version='0.1.0',
     description="Mews HoKo Reporter",
     author="Dominik Neise",
     author_email='neised@phys.ethz.ch',
