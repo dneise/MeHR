fc089daa2ef077703738623a82dedefe7eb62138
Dominik Neise <neised@phys.ethz.ch>
2018-07-26 19:44:11 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MeHR',
-    version='0.4.1',
+    version='0.4.4',
     description="Mews HoKo Reporter",
     author="Dominik Neise",
     author_email='neised@phys.ethz.ch',
